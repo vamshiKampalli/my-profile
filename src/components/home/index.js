34e5632050c10cecf15172906e7fee2f17434818
vamshiKampalli <vamshi.vshs@gmail.com>
2023-12-30 10:41:26 +0530

    Add profile image

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,6 +3,7 @@ import { MdOutlineContactMail } from 'react-icons/md';
 import { GrContactInfo } from 'react-icons/gr';
 import { CgWebsite } from 'react-icons/cg';
 import { useSelector } from 'react-redux';
+import vamPng from './vam.png';
 
 import styles from './home.module.css';
 
@@ -23,21 +24,20 @@ function Home() {
       <div className={styles.top_container}>
         <div className={styles.img}>
           <svg
-            fill="#506bf2"
-            width="500px"
-            height="500px"
+            fill="#fff"
             viewBox="60 0 300 350"
             xmlns="http://www.w3.org/2000/svg"
-            stroke="#506bf2"
+            stroke="#fff"
           >
-            <g id="SVGRepo_bgCarrier" strokeWidth="0">
+            <mask id="myMask">
               <path
                 transform="translate(-25.6, -25.6), scale(13)"
                 d="M16,28.16078191064298C19.77806540407412,28.257196759095947,23.265226910282255,26.60541331875971,25.894271714641818,23.890418394798722C28.940259032533277,20.744850052710532,32.359440731676266,16.748325022027444,31.11690748273453,12.549664514106217C29.905302559464026,8.455514319838606,24.694878092705082,7.736123583731422,20.68203017069486,6.277664171154905C17.397036675515338,5.083741606330381,13.907916388483518,3.6007313602089606,10.77866245917405,5.157780877748529C7.683344072607544,6.6979447983857625,6.565703891940979,10.30049901215235,5.814777804613376,13.675289498689285C5.0707556281069115,17.01905262720488,4.749032890573651,20.598899940118653,6.719337857239598,23.40108109005977C8.82323061509933,26.39325170695073,12.343397476064915,28.067466750503677,16,28.16078191064298"
-                fill="#506bf2"
+                fill="#fff"
                 strokeWidth="0"
               ></path>
-            </g>
+            </mask>
+            <image href={vamPng} mask="url(#myMask)"></image>
           </svg>
         </div>
         <div className={styles.intro}>
